e8e31e5614261fb5399325d973dd5c8cca58f2a7
CJ Erskine <cjerskine@gmail.com>
2019-02-15 14:41:04 -0500

    Large Changes from Other PC

--- main.go
+++ main.go
@@ -5,21 +5,33 @@ import (
 	"flag"
 	"fmt"
 	"lib"
+	"math"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 
 	_ "github.com/bmizerany/pq"
-
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
 	// Token export
 	Token string
+	//pqueue                       map[Player]int
+	player1elo, player1pc        int
+	player2elo, player2pc        int
+	winner, finallelo, finalwelo int
+	name1, name2                 string
 )
+
+// Player object
+type Player struct {
+	Name string
+	Elo  int
+}
 
 const (
 	host     = lib.ElosuHost
@@ -31,12 +43,12 @@ const (
 )
 
 func init() {
-
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
 func main() {
+
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
@@ -66,7 +78,6 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
@@ -86,15 +97,104 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 		if strings.HasSuffix(m.Content, "stats") {
-			var message string
-			message = getUserInfo(m.Author.ID)
-			s.ChannelMessageSend(m.ChannelID, message)
+			out := printUserInfo(m.Author.ID)
+			s.ChannelMessageSend(m.ChannelID, out)
+		}
+
+		if strings.Contains(m.Content, "create") {
+			if isExisting(m.Author.ID) {
+				s.ChannelMessageSend(m.ChannelID, "You already have an account!")
+			} else if strings.HasSuffix(m.Content, "create") {
+				s.ChannelMessageSend(m.ChannelID, "Please add your osu account ID after the create command!")
+			} else {
+				str := strings.SplitAfterN(m.Content, " ", 2)
+				if _, err := strconv.Atoi(str[1]); err == nil {
+					newUser(str[1], m.Author.Username, m.Author.ID)
+					s.ChannelMessageSend(m.ChannelID, "Your account has been created, if your username on osu is not the same as your discord username change it with \"e!namechange [username]\".")
+				} else {
+					s.ChannelMessageSend(m.ChannelID, "I don't think that is an ID, make sure to use your ID and not your username!")
+				}
+
+			}
+		}
+
+		if strings.Contains(m.Content, "namechange") {
+			str := strings.SplitAfterN(m.Content, " ", 2)
+			changeName(str[1], m.Author.ID)
+
+			_, name, _, _, _, _ := getUserInfo(m.Author.ID)
+			s.ChannelMessageSend(m.ChannelID, name)
+		}
+
+		if strings.Contains(m.Content, "submit") {
+			str := strings.Split(m.Content, " ")
+			matchResults(str[1], str[2])
 		}
 	}
 
 }
 
-func getUserInfo(id string) string {
+func changeName(name, id string) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//Pings to check the connection
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	//Test to add my user to the db
+	_, err = db.Exec("UPDATE player SET name = $1 WHERE discid = $2", name, id)
+	checkErr(err)
+}
+
+func matchResults(idWin, idLose string) {
+	var (
+		welo, wwins, wlosses int
+		lelo, lwins, llosses int
+	)
+	_, _, welo, wwins, wlosses, _ = getUserInfo(idWin)
+	_, _, lelo, lwins, llosses, _ = getUserInfo(idLose)
+
+	elow, elol := calcK(1, welo, lelo, (wwins + wlosses), (lwins + llosses))
+
+	updatePlayerElo(elow, idWin, true)
+	updatePlayerElo(elol, idLose, false)
+
+}
+
+func updatePlayerElo(elo int, id string, win bool) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	//Pings to check the connection
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	if win {
+		_, err = db.Exec("UPDATE player SET elo = $1, wins = wins + 1 WHERE discid = $2", strconv.Itoa(elo), id)
+		checkErr(err)
+	} else {
+		_, err = db.Exec("UPDATE player SET elo = $1, losses = losses + 1 WHERE discid = $2", strconv.Itoa(elo), id)
+		checkErr(err)
+	}
+
+}
+
+func printUserInfo(id string) string {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
@@ -132,7 +232,41 @@ func getUserInfo(id string) string {
 
 }
 
-func checkExisting(id string) bool {
+func getUserInfo(id string) (int, string, int, int, int, int) {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	checkErr(err)
+	defer db.Close()
+	//Pings to check the connection
+	err = db.Ping()
+	checkErr(err)
+
+	//Test to read the users from the db
+	userinf, err := db.Query("SELECT * FROM player WHERE discid = $1", id)
+	checkErr(err)
+
+	var (
+		playerid int
+		name     string
+		elo      int
+		wins     int
+		losses   int
+		joindate time.Time
+		discid   int
+	)
+	for userinf.Next() {
+		err = userinf.Scan(&playerid, &name, &elo, &wins, &losses, &joindate, &discid)
+		checkErr(err)
+	}
+
+	return playerid, name, elo, wins, losses, discid
+
+}
+
+func isExisting(id string) bool {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
@@ -148,8 +282,6 @@ func checkExisting(id string) bool {
 	userinf, err := db.Query("SELECT name FROM player WHERE discid = $1", id)
 	checkErr(err)
 
-	var out string
-
 	var (
 		name string
 	)
@@ -158,16 +290,15 @@ func checkExisting(id string) bool {
 		checkErr(err)
 	}
 
-	out = fmt.Sprintf("User: %v", name)
-	if out == "" {
-		return true
+	if name == "" {
+		return false
 	}
 
-	return false
+	return true
 
 }
 
-func newUser(id, name, elo, discordID string) {
+func newUser(str, name, discordID string) {
 
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
@@ -184,12 +315,85 @@ func newUser(id, name, elo, discordID string) {
 	}
 
 	//Test to add my user to the db
-	_, err = db.Exec("INSERT INTO player (playerid, name, elo, wins, losses, joindate, discid) VALUES VALUES($1, $2, $3, 0, 0, current_timestamp, $4)",
-		id, name, elo, discordID)
-
+	_, err = db.Exec("INSERT INTO player (playerid, name, elo, wins, losses, joindate, discid) VALUES ( $1, $2, 1200, 0, 0, current_timestamp, $3)", str, name, discordID)
 	checkErr(err)
 
 }
+
+// Constantly running method that finds 2 players that have been added to the queue that are near enough to each other in rank and pairs them together.
+// func playerQueue() {
+// 	for true {
+// 		//example player Pupper
+// 		pupper := Player{
+// 			Name: "Pupper",
+// 			Elo:  1500,
+// 		}
+// 		pqueue[pupper] = 0
+
+// 		sort.Sort(pqueue)
+
+// 		//wait 2 seconds
+// 		time.Sleep(2 * time.Second)
+// 	}
+// }
+
+// ********** START ELO CALCULATOR **********
+// Does calculatons to find the correct elo of the 2 players and returns an HTML string with the new values
+func calcElo(welo, lelo, wk, lk int) (int, int) {
+	// Fuck what does this do again
+	var wdiv = float64(float64(welo) / 400.0)
+	var rw = float64(math.Pow(10, wdiv))
+	var ldiv = float64(float64(lelo) / 400.0)
+	var rl = float64(math.Pow(10, ldiv))
+	finalwelo = welo + int(float64(wk)*(1-(rw/(rw+rl))))
+	finallelo = lelo + int(float64(lk)*(0-(rl/(rw+rl))))
+	//return formatted elo values
+	return finalwelo, finallelo
+}
+
+// Calculates the K value and calls calcElo using the correct winner/loser ordering
+func calcK(winner, player1elo, player2elo, player1pc, player2pc int) (int, int) {
+	var wk, lk int
+	var newK = 75
+	var oldK = 25
+	// Logic for faster new player growth
+	if player1pc <= 10 {
+		if winner == 1 {
+			wk = newK
+		} else {
+			lk = newK
+		}
+	} else if player1pc > 10 {
+		if winner == 1 {
+			wk = oldK
+		} else {
+			lk = oldK
+		}
+	}
+	if player2pc <= 10 {
+		if winner == 2 {
+			wk = newK
+		} else {
+			lk = newK
+		}
+	} else if player2pc > 10 {
+		if winner == 2 {
+			wk = oldK
+		} else {
+			lk = oldK
+		}
+	}
+	// Choses correct order for player that won.
+	if winner == 1 {
+		w, l := calcElo(player1elo, player2elo, wk, lk)
+		return w, l
+	}
+	w, l := calcElo(player2elo, player1elo, wk, lk)
+	return w, l
+
+}
+
+// ********** STOP ELO CALCULATOR **********
 
 //Stops bot and displays error in console
 func checkErr(err error) {
